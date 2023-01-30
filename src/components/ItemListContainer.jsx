3137d8fe4f5283b6fe1338e5b7e13cd4cce9819e
Xavier Martinuzzi <xaviermagg@gmail.com>
2023-01-30 09:54:47 -0700

    arreglo de props

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,12 +1,12 @@
 import { Container, Flex, Spacer, Box } from '@chakra-ui/react';
-const ItemListContainer = ({saludo, subSaludo}) => {
+const ItemListContainer = (props) => {
   return (
     <div>
       <Container className="saludoPrincipal" maxW="70rem">
         <Flex justifyContent="center">
           <Box className='banner'>
-            <h1 className="bienvenido">{saludo}</h1>
-            <h2 className="subBienvenido">{subSaludo}</h2>
+            <h1 className="bienvenido">{props.saludo}</h1>
+            <h2 className="subBienvenido">{props.subSaludo}</h2>
           </Box>
         </Flex>
       </Container>
